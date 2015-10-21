a330d168d190d7c79292f68386ddddec333e4423
somename <someemail>
2015-10-21 15:42:51 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -4,7 +4,35 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 // ID:
 // Finish time:
 public class PlanetExplorer {
+	int[][] grid;
+	int x;
+	int y;
+	String obstacles;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
+		this.x=x;
+		this.y=y;
+		this.obstacles=obstacles;
+		
+	
+		grid=new int[x][y];
+		for(int sizeOfString=0; sizeOfString<=obstacles.length(); sizeOfString++){
+			
+			if(obstacles.charAt(sizeOfString)==')'){
+				//System.out.println(obstacles.charAt(sizeOfString-3) + "," + obstacles.charAt(sizeOfString-1));
+				grid[obstacles.charAt(sizeOfString-3)][obstacles.charAt(sizeOfString-1)]=2;
+			}
+		}
+		
+		
+		for(int i=0; i<x;i++){
+			for(int j=0; j<y;j++){
+				if(grid[i][j]!=2){
+					grid[i][j]=0;
+				}
+			}
+		}
+			
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
 	 *  
@@ -13,6 +41,47 @@ public class PlanetExplorer {
 	 */
 	}
 	
+	
+	public int[][] getGrid() {
+		return grid;
+	}
+
+
+	public void setGrid(int[][] grid) {
+		this.grid = grid;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+
+	public String getObstacles() {
+		return obstacles;
+	}
+
+
+	public void setObstacles(String obstacles) {
+		this.obstacles = obstacles;
+	}
+
+
 	public String executeCommand(String command){
 		
 		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
@@ -26,5 +95,13 @@ public class PlanetExplorer {
 		 */
 		
 		return null;
+	}
+	public void displayGrid(){
+		for(int i=0; i<x;i++){
+			for(int j=0; j<y;j++){
+				System.out.print(this.grid[i][j]);
+			}
+			System.out.println("\n");
+		}
 	}
 }
--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -1,13 +1,18 @@
 package org.unioulu.tol.sqat2015.planetExplorer.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testClass() {
+		PlanetExplorer pe=new PlanetExplorer(10,10,"(5,5)(7,8)");
+		
+		pe.displayGrid();
+		assertTrue(pe.getGrid()[5][5]==2);
+		
 	}
 }
