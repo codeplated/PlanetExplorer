4435ee119ecbfa5b21de4d15fae8c5fdecdcba85
somename <someemail>
2015-10-21 16:15:08 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -22,7 +22,7 @@ public class PlanetExplorer {
 		}
 		
 		obstacles = obstacles.replaceAll("\\D+","");
-		System.out.println(obstacles.length());
+		System.out.println(obstacles.charAt(2));
 		for(int sizeOfString=0; sizeOfString<=obstacles.length(); sizeOfString++){
 			
 			grid[obstacles.charAt(sizeOfString)][obstacles.charAt(sizeOfString)+1]=2;
